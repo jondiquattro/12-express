11abc2cec7f331f35c19740f0f9df006fd8f1f08
Jonathan DiQuattro <jondiquattro@hotmail.com>
2019-01-27 18:06:54 -0800

    got server modular and working, with tests passing

---
+++ models/products.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const uuid = require('uuid/v4');
+
+const ProductsModel = require('./products-schema');
+const schema = {
+};
+
+class Products {
+
+  constructor() {
+  
+  }
+
+  get(_id) {
+    let queryObject = _id ? {_id} : {};
+    return ProductsModel.find(queryObject);
+    }
+  
+  post(record) {
+    console.log(record);
+    let newRecord = new ProductsModel(record);
+    return newRecord.save();
+
+
+  }
+
+  put(_id, record) {
+    let queryObject = _id ? {_id} : {};
+    return ProductsModel.findOneAndReplace(queryObject, {record})
+
+ 
+  }
+
+  delete(_id) {
+    console.log(_id)
+    let queryObject = _id ? {_id} : {};
+    return ProductsModel.findOneAndDelete(queryObject);
+  }
+
+  
+
+}
+
+module.exports = Products;
